9903ff6fc506623bb446e0a1f885d2d78e2ae53d
Spencer Lyon <spencerlyon2@gmail.com>
2020-06-10 11:06:23 -0400

    STY: remove `return` from examples

--- examples.jl
+++ examples.jl
@@ -6,45 +6,45 @@ c = Client()
 
 function multiple_dataset_all(c=Client())
     request!(c, demographics(), covid())
-    return fetch(c)
+    fetch(c)
 end
 
 
 function multiple_dataset_states_only(c=Client())
     request!(c, demographics(), covid(fips="<100"))
-    return fetch(c)
+    fetch(c)
 end
 
 
 function multiple_dataset_counties_only(c=Client())
     request!(c, demographics(), covid(fips=">1000"))
-    return fetch(c)
+    fetch(c)
 end
 
 
 function single_dataset_all(c=Client())
     request!(c, mobility_devices())
-    return fetch(c)
+    fetch(c)
 end
 
 
 function single_dataset_deaths_filter(c=Client())
     request!(c, covid(fips="<100", variable="deaths_total", value=">100"))
 
-    return fetch(c)
+    fetch(c)
 end
 
 
 function single_dataset_multiplestatesallcounties(c=Client())
     request!(c, mobility_devices(state=[6, 48]))
 
-    return fetch(c)
+    fetch(c)
 end
 
 
 function single_dataset_onestateallcounties(c=Client())
     request!(c, mobility_devices(state=6))
-    return fetch(c)
+    fetch(c)
 end
 
 
@@ -59,5 +59,5 @@ function single_dataset_variableselect(c=Client())
         )
     )
 
-    return fetch(c)
+    fetch(c)
 end
